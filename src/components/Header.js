243fb4fe7e92ac0570438677e76236ed82fe7853
Jeanne <j.pietropaoli@yahoo.fr>
2023-07-18 21:02:10 +0200

    Add styling update on location change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logoLight from "../assets/logo.png"
 import logoDark from "../assets/title-logo-black.png"
 import Navbar from "./Navbar";
 import "../styles/Header.css"
 
 export default function Header(props) {
-    const styles = {
-        color: props.color,
-        backgroundColor: props.backgroundColor,
-        boxShadow : props.boxShadow
+    const [test,setTest] = React.useState(true)
+    const homeHeaderStyles = {
+        logo : "light",
+        color : "white",
+        backgroundColor : "transparent",
+        boxShadow : "none"
+    }
+    const shopHeaderStyles = {
+        logo : "light",
+        color : "white",
+        backgroundColor : "#036B66", 
+        boxShadow : "2px 2px 2px lightgrey"
+    }
+    const location = useLocation()
+    const [styles, setStyles] = React.useState(getRightStyles())
+
+    React.useEffect(() => {
+        setStyles(getRightStyles())
+    },
+    [location.pathname]) 
+
+    function getRightStyles() {
+        const isHomeActive = location.pathname === "/"
+        return isHomeActive ? homeHeaderStyles : shopHeaderStyles
     }
 
     return (
-        <header style={styles}>
+        <header style={styles} >
             <div className="header--logo-container">
                 <img className="logo-img" alt="Bean Barn logo" src={props.logo === "dark" ? logoDark : logoLight} />
                 <h3 className="logo-text">BEAN BARN</h3>
                 <Link to="/"></Link>
             </div>
-            <Navbar />
+            <Navbar openCart={props.openCart} />
         </header>
     )
-}
-
-Header.defaultProps = {
-    logo : "light",
-    color: "white",
-    backgroundColor: '#036B66',
-    boxShadow : "2px 2px 2px lightgrey"
 }
