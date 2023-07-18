e9013fbff49c1a4f69c91257038f0abd2bdfde3a
Jeanne <j.pietropaoli@yahoo.fr>
2023-07-18 21:14:51 +0200

    Add Header component rendering in the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './styles/App.css';
+import React from 'react';
+import Cart from './components/Cart';
+import Header from './components/Header';
 import RouteSwitch from './components/RouteSwitch';
+import Navbar from './components/Navbar';
 
 
 function App() {
+  React.useEffect(() => console.log('apprenders')) 
+  const [cartShown, setCartShown] = React.useState(true)
+
+  function closeCart() {
+    setCartShown(false)
+  }
+
+  function openCart() {
+    setCartShown(true)
+  }
+
   return (
     <div className="app">
+      <Header openCart={openCart} />
       <RouteSwitch />
+      {cartShown && <Cart closeCart={closeCart} />}
     </div>
   );
 }
