fd84a32608f031718f5fc5b63c17edc8df0a3c80
Jeanne <j.pietropaoli@yahoo.fr>
2023-07-18 21:11:35 +0200

    Add basic cart state

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,25 @@
 import React from "react";
+import "../styles/Cart.css"
 
-export default function Cart() {
+export default function Cart(props) {
+    const [cartItems, setCartItems] = React.useState([])
     const cartItemsElements = []
+    
     return (
-        <div className="cart">
-            <h2>Cart</h2>
-            <div className="cart--items-container">
-                {cartItemsElements}
+        <div className="cart-container">
+            <div className="cart">
+                <h2>Cart</h2>
+                <div className="cart--items-container">
+                    {cartItems.length === 0 && <p>- No item in your cart yet -</p>}
+                    {cartItemsElements}
+                </div>
+                <div className="cart--total-container">
+                    <h2>Total</h2>
+                    <h2>0$</h2>
+                </div>
+                <button className="cart--checkout-button">Checkout</button>
+                <button onClick={props.closeCart} className="cart--close-button">x</button>
             </div>
-            <div className="cart--total-container">
-                <h2>Total</h2>
-                <h2>0$</h2>
-            </div>
-            <button className="cart--checkout-button">Checkout</button>
-            <button className="cart--close-button">x</button>
         </div>
     )
 }
